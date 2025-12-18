a382b079762227cf6575f1420edd1fcfd3aaf05a
Sadik Al Jarif <sadikaljarif05@gmail.com>
2025-12-18 17:57:24 +0600

    Add files via upload

---
+++ 2.variable.js
@@ -0,0 +1,31 @@
+// string
+var friend_name="sadik_al Jarif";
+console.log(friend_name);
+console.log(typeof friend_name);
+//number
+var my_number=12344;
+console.log(my_number);
+console.log(typeof my_number);
+
+// boolean
+var bool_type=true;
+console.log(bool_type);
+console.log(typeof bool_type);
+
+var bool_type=false;
+console.log(bool_type);
+console.log(typeof bool_type);
+
+// uppercase
+var text="bangladesh is a poor country.it has large population also most of the people are uneducated";
+console.log(text.toUpperCase());
+//lowercase
+var low_text="BANGLADESH IS A POOR COUNTRY.IT HAS LARGE POPULATION ALSO MOST OF THE PEOPLE ARE UNEDUCATED";
+console.log(low_text.toLocaleLowerCase());
+//split
+var friend="bangla, english, math, chemisty, physics, history";
+console.log(friend.split("bangla"));
+
+// indexof
+console.log(friend.indexOf("math"));
+
