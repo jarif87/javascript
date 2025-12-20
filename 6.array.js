a963a9d792592a3040e959100f8a1157fb3f12b6
Sadik Al Jarif <sadikaljarif05@gmail.com>
2025-12-20 17:09:04 +0600

    Add files via upload

---
+++ 6.array.js
@@ -0,0 +1,28 @@
+var array_name = ["bangla", "english", "Math", "physics", "chemistry"];
+console.log(array_name[2]);
+
+array_name[2] = "bangladesh";
+console.log(array_name);
+
+var array_name = ["bangla", "english", "Math", "physics", "chemistry"];
+array_name.push("hello_world");
+console.log(array_name);
+
+array_name.pop();
+console.log(array_name);
+
+var array_name = ["bangla", "english", "Math", "physics", "chemistry"];
+array_name.shift();
+console.log(array_name);
+
+var array_name = ["bangla", "english", "Math", "physics", "chemistry"];
+array_name.unshift("hello_coxsBazar");
+console.log(array_name);
+
+var array_name = ["bangla", "english", "Math", "physics", "chemistry"];
+var total = array_name.slice(0, 2);
+console.log(total);
+
+var friend_name = ["shakib", "tamim", "musfiq", "riyad", "masrafi"];
+friend_name.splice(0, 2, "shanto");
+console.log(friend_name);
---
+++ 7.for loop.js
@@ -0,0 +1,17 @@
+for (var sum = 1; sum < 20; sum++) {
+  console.log("hello_world");
+}
+
+for (var sum = 0; sum < 5; sum++) {
+  console.log(sum);
+}
+
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, "shakib", "tamim", "riyad"];
+for (var i = 0; i < number.length; i++) {
+  console.log(number[i]);
+}
+
+var i = 0;
+while (i < 10) {
+  console.log("Hi Babu");
+}
