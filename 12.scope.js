ebf15f2cfa3993c769a1cf66e70ff1f20fb89851
Sadik Al Jarif <sadikaljarif05@gmail.com>
2025-12-21 18:39:31 +0600

    Add files via upload

---
+++ 12.scope.js
@@ -0,0 +1,13 @@
+var fnd_name = "shakib";
+
+function hello() {
+  var a = "hello_world";
+  console.log(a);
+}
+hello();
+
+let hi;
+{
+  hi = "bangla is a language";
+}
+console.log(hi);
---
+++ 13.hosting.js
@@ -0,0 +1,20 @@
+let name = "riyad";
+name = "hello world";
+console.log(name);
+
+var expression = function (message1, message2) {
+  console.log(message1, message2);
+};
+expression("helloworld", "hibangladesh");
+
+function world() {
+  var hello = "bangladesh";
+  function helloworld() {
+    console.log(hello);
+    let h = "javascript";
+    console.log(h);
+  }
+  helloworld();
+}
+
+world();
