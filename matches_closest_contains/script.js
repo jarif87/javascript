ede320fefe5fcd313fae41dfe0d1a85096601a62
Sadik Al Jarif <sadikaljarif05@gmail.com>
2025-12-29 20:22:17 +0600

    seventh

---
+++ matches_closest_contains/script.js
@@ -0,0 +1,10 @@
+let id1 = document.getElementById("id1");
+console.log(id1);
+console.log(id1.matches(".box"));
+
+//closest
+console.log(span1.closest("#span1"));
+
+//contains
+let sp1 = document.getElementById("span1");
+console.log(sp1.contains(span1));
