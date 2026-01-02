a987535679e2ea44b898ffb81d85dca46cd7a4f8
Sadik Al Jarif <sadikaljarif05@gmail.com>
2026-01-03 00:49:46 +0600

    9th

--- question/script.js
+++ question/script.js
@@ -1,7 +1,25 @@
-let age = prompt("Enter your age: ");
-age = parseInt(age);
-if (age > 10 && age < 20) {
-  alert("your age is correct");
-} else {
-  alert("age is not correct");
-}
+// let age = prompt("Enter your age: ");
+// age = parseInt(age);
+// if (age > 10 && age < 20) {
+//   alert("your age is correct");
+// } else {
+//   alert("age is not correct");
+// }
+
+/*
+document.getElementById("goole").addEventListener("click", function () {
+  let url = "http://google.com";
+  window.location = url;
+});
+
+setInterval(async function () {
+  let url = "https://jsonplaceholder.typicode.com/todos/1";
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+}, 3000);
+*/
+
+setInterval(async function () {
+  document.querySelector("#hello").classList.toggle("hello");
+}, 300);
