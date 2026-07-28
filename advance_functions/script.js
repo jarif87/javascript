219b1922efb1323a5364e86b451b8c6bba6584f0
Sadik Al Jarif <sadikaljarif05@gmail.com>
2026-07-28 19:30:50 +0600

    45th

---
+++ advance_functions/script.js
@@ -0,0 +1,28 @@
+// function greet(name) {
+//   console.log("Hello, " + name + "!");
+// }
+
+// greet("Alice");
+
+// const number = 10;
+// const function_1 = function greet() {
+//   console.log("Hello, World!");
+// };
+
+// console.log(function_1);
+// console.log(typeof function_1);
+
+// const function_2 = function (name) {
+//   console.log("Hello, " + name + "!");
+// };
+
+// function_2("Bob");
+
+const object1 = {
+  Number: 2,
+  fun: function greet() {
+    console.log("Hello, World!");
+  },
+};
+
+object1.fun();
