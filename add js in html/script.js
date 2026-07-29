005cdcb3fc157e4412a5a6ddde383fa62583785d
Sadik Al Jarif <sadikaljarif05@gmail.com>
2026-07-29 22:52:12 +0600

    50th

--- add js in html/script.js
+++ add js in html/script.js
@@ -1,5 +1,9 @@
-alert("hello bangladesh");
+// alert("hello bangladesh");
 //multiple comment
 /*
 alert("hello bangladesh is a small cuuntry");
 */
+
+// document.write("hello bangladesh is a small country");
+
+console.log("hello bangladesh is a Big country");
