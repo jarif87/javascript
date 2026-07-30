7e7c0a3cd0c119e0ab7e18ba84c4a2f91b27d9a2
Sadik Al Jarif <sadikaljarif05@gmail.com>
2026-07-30 23:18:41 +0600

    51th

--- add js in html/script.js
+++ add js in html/script.js
@@ -6,4 +6,28 @@ alert("hello bangladesh is a small cuuntry");
 
 // document.write("hello bangladesh is a small country");
 
-console.log("hello bangladesh is a Big country");
+// console.log("hello bangladesh is a Big country");
+
+// script.js
+// const name = "Bangladesh";
+
+// // Target the element by its ID and change its text content
+// document.getElementById("country-heading").innerText =
+//   `hello ${name} is a small country`;
+
+let number1 = 10.0122345;
+console.log(number1.toPrecision(2)); // Output: 10
+console.log(number1.toPrecision(3));
+console.log(number1.toPrecision(4));
+console.log(number1.toPrecision(5));
+console.log(number1.toFixed(2)); // Output: 10.01
+console.log(number1.toFixed(3)); // Output: 10.012
+console.log(number1.toFixed(4)); // Output: 10.0122
+console.log(number1.toFixed(5)); // Output: 10.01223
+
+console.log("Exponential Notation:");
+let number2 = 1234.56789;
+console.log(number2.toExponential(2));
+// console.log(number2.toExponential(3));
+// console.log(number2.toExponential(4));
+// console.log(number2.toExponential(5));
