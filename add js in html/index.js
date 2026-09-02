620f56fce3f116b57ad7eb439fd1557224c151ee
Sadik Al Jarif <sadikaljarif05@gmail.com>
2026-09-03 01:29:29 +0600

    84th

--- add js in html/index.js
+++ add js in html/index.js
@@ -1,15 +1,30 @@
 let f = require("./friends.json");
-console.log("===================================");
-console.log(f);
-console.log(f.friends[0]);
-console.log("===================================");
-console.log(f.friends[0].name);
-console.log("===================================");
-f.friends[0].name = "John Doe";
-console.log(f.friends[0].name);
-console.log("===================================");
-console.log(f);
-console.log("===================================");
-console.log(f.friends[1].name);
-console.log("===================================");
-console.log(f.friends[1].age);
+// console.log("===================================");
+// console.log(f);
+// console.log(f.friends[0]);
+// console.log("===================================");
+// console.log(f.friends[0].name);
+// console.log("===================================");
+// f.friends[0].name = "John Doe";
+// console.log(f.friends[0].name);
+// console.log("===================================");
+// console.log(f);
+// console.log("===================================");
+// console.log(f.friends[1].name);
+// console.log("===================================");
+// console.log(f.friends[1].age);
+
+// for (let i = 0; i < f.friends.length; i++) {
+//   console.log(f.friends[i].name);
+//   console.log(f.friends[i].age);
+//   console.log(f.friends[i].city);
+
+//   console.log("===================================");
+// }
+/*
+let x = 0;
+for (x in f) {
+  console.log(x);
+  console.log(f[x]);
+}
+*/
